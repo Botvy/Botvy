ae12a23e8578d3f3659de2d7b31aa75d860fc346
Yannick Fricke <yannickfricke@googlemail.com>
2021-02-28 02:43:25 +0100

    Added the basic entrypoint for the UI
    - Added the App component
    - Added the index.html file
    - Added the main entrypoint for the UI (main.tsx)
    - Added the basic webpack configuration for the UI package

---
+++ packages/apps/ui/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.tsx',
+    mode: process.env.NODE_ENV !== 'development' ? 'production' : 'development',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    transpileOnly: true,
+                    experimentalWatchApi: true,
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        plugins: [new TsconfigPathsPlugin()],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Botvy',
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: path.resolve(__dirname, 'dist', 'index.html'),
+        }),
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9000,
+        hot: true,
+    },
+};
