fc9a813f18180b4df2e1323d1f578e1d89d463c1
Yannick Fricke <yannickfricke@googlemail.com>
2021-03-14 01:59:17 +0100

    Jest should only check typescript files

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     roots: ['./packages/apps/ui/src/', './packages/libs/plugin-system/src/'],
-    testRegex: '(/__tests__/.*).test.[jt]sx?$',
+    testRegex: '(/__tests__/.*).test.tsx?$',
     setupFiles: ['./setup-jest.ts'],
     globals: {
         'ts-jest': {
