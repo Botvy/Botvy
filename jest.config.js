6d4d068bb3a2ca2c5d8e90b0b62349280f91ea80
Yannick Fricke <yannickfricke@googlemail.com>
2021-03-14 04:33:26 +0100

    Updated the code coverage collection rules
    - Include all source files
    - Exclude all test files

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,10 @@ module.exports = {
     testRegex: '(/__tests__/.*).test.tsx?$',
     setupFiles: ['./setup-jest.ts'],
     coverageReporters: ['html', 'lcov'],
+    collectCoverageFrom: [
+        './packages/**/src/**/*.{ts,tsx}',
+        '!./packages/**/src/**/__tests__/**/*.{ts,tsx}',
+    ],
     globals: {
         'ts-jest': {
             tsconfig: './tsconfig.base.json',
